6f0d5362b0a509f73fd8e0dc3491982a65e03198
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-04-04 14:51:59 -0400

    tested get and post, but nothing else is connected

--- public/server.js
+++ public/server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const notes = require('../db/db.json')
+
 
 const app = express()
 const PORT = process.env.PORT || 7500
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
 
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db.json')))
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
 
 app.post('/api/notes', (req, res) => {
-})
+    res.send(req.body);
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
