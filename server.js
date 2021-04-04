edb3a0c04bb2ada8a137328bb8ed31f29c0de70d
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-04-04 18:52:05 -0400

    fixed file structure

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
-const notes = require('../db/db.json')
+const notes = require('./db/db.json')
 
 const app = express()
 const PORT = process.env.PORT || 7500
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(express.static('/public'));
-
-app.use('js', function (req, res) {
-    res.sendFile(path.join(__dirname,'/index.js'))
-});
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile('index.html');
 });
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'notes.html'))
-)
+app.get('/notes', (req, res) => {
+  res.sendFile(__dirname + '/public/notes.html');
+});
 
 app.get('/api/notes', (req, res) => {
   res.json(notes)
