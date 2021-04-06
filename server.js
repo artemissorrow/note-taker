9dfb111d19f017d9c3278496b9c0efc093f485ae
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-04-05 20:08:08 -0400

    it does everything except delete

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express')
-const path = require('path')
 const fs = require('fs')
 const notes = require('./db/db.json')
+const uniqueSlug = require('unique-slug')
 
 const app = express()
 const PORT = process.env.PORT || 7500
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
-});
+  res.sendFile('index.html')
+})
 
 app.get('/notes', (req, res) => {
-  res.sendFile(__dirname + '/public/notes.html');
-});
+  res.sendFile(__dirname + '/public/notes.html')
+})
 
 app.get('/api/notes', (req, res) => {
   res.json(notes)
@@ -24,6 +24,17 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
   res.send(req.body)
+
+  let newNote = req.body
+  newNote.id = uniqueSlug();
+
+  notes.push(newNote);
+
+  noteString = JSON.stringify(notes);
+
+  fs.appendFile('./db/db.json', noteString, err => {
+    if (err) throw err
+  })
 })
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
